ed476a22c686ed964d8ecc50fedc1c2524261a8c
AreonL <areon.lundkvist@gmail.com>
2022-01-26 05:06:12 +0100

    fix: database can get actual values

--- api/fakeapi/index.js
+++
@@ -1,9 +0,0 @@
-db = require('./db.json')
-students = require('./students.json')
-teachers = require('./teachers.json')
-
-module.exports = () => ({
-  db: db,
-  students: students,
-  teachers: teachers
-})
